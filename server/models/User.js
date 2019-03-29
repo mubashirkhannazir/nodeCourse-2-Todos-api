8f7a6574ce49b6fc802c080263a31bbcfa243377
Muhamma Mubashir Nazir <mubashirkhannazir1@google.com>
2019-03-29 04:43:45 -0700

    Add GET/Todo/:id

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,40 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate:{ 
+            // validator: (value) => {
+            //     return validator.isEmail(value);
+            // },
+            validator: validator.isEmail,
+            message: '{value} is not email'
+        }   
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+
+    },
+
+    tockens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        tocken: {
+            type: String,
+            required: true
+        }
+    }]
+
 });
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var bodyParser  = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/Todo.js');
 var {User} = require('./models/User.js');
-
+var {ObjectID} = require('mongodb');
 var app = express();
 
 app.use(bodyParser.json());
@@ -21,6 +22,33 @@ app.post('/todos', (req, res) => {
         res.status(400).send(err);
     })
 });
+
+app.get('/todos', (req,res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+        return res.status(404).send();
+    };
+    res.send({todo});
+    }).catch(
+
+        (err) => {
+           return res.status(400).send()
+        });
+    }
+);
 app.listen(3000, () => {
     console.log('connected at port 3000')
 });
